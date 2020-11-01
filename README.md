da9a44cd327ec3a2ff955fd759fd6b50ec0d9e0d
NDPenaloza <ndpenaloza@gmail.com>
2020-10-31 23:32:29 -0700

    wrote readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# homework5
+# homework5 - Work Day Scheduler
+
+Description
+
+This project requires building a work schedule planner. Using moment.js and utilizing the CSS file, the current hour will highlight red, future hours will be green, and past hours will turn gray. Appointments can be saved in each hour and saved in local storage.
+
+Links
+
+Application URL: https://ndpenaloza.github.io/homework5/
+
+GitHub Repository URL: https://github.com/ndpenaloza/homework5
+
+
+License
+MIT License
+
+
+
+Copyright (c) 2020 Nick D. Penaloza
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-
-
 $(document).ready(function() {
     // Current day and time in Jumbotron
     $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
     // For loop iterating rows in .container div
-    for (let i = 9; i < 18; i++) {
+    for (var i = 9; i < 18; i++) {
         // Create hourly-row
-        var hourlyRow = $('<div data-time=`${i}` id=`${i}` class="hourly-row">');
+        var hourlyRow = $(`<div data-time=${i} id='${i}' class="hourlyRow">`);
         // Create first column for each hour
-        var hour = $('<div class="col-sm-1"> <p class="hour">' + formatAMPM(i) + '</p>');
+        var hour = $('<div class="col-sm-1"> <p class="hour">' + ampmConvert(i) + '</p></div>');
         // Create second column to record appointments
-        var appointment = $('<div class="col-sm-10 past"><textarea id=text${i} class="appointment"></textarea>');        
+        var appointment = $(`<div class="col-sm-10 past"><textarea id=text${i} class="appointment" placeholder="Add appointment..."></textarea></div>`);      
         // Create third column for save button
-        var saveBtn = $('<div class="col-sm-1"><button class="saveBtn" id=`${i}`><i class="fas fa-save"></i></button>');
+        var saveBtn = $(`<div class="col-sm-1"><button class="saveBtn" id=${i}><i class="fas fa-save"></i></button></div></div>`);
         // append hour
         hourlyRow.append(hour);
         //append appointment
@@ -21,7 +19,46 @@ $(document).ready(function() {
         hourlyRow.append(saveBtn);
         // append hourlyRow to .container div
         $(".container").append(hourlyRow);
+
+        getLocalStorage(i);
+
     }
 
+    function ampmConvert(hours) {
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        return hours + ampm;
+    }
 
+    ampmConvert();
+
+    function updateColors(){
+        var currentTime = new Date().getHours();
+        for (var i = 9; i < 18; i++) {
+            if ($(`#${i}`).data("time") == currentTime){
+                $(`#text${i}`).addClass( "present");
+            } else if (currentTime < $(`#${i}`).data("time")) {
+                $(`#text${i}`).addClass( "future");
+            }
+        }
+    }
+
+    setInterval(function() {
+        updateColors();
+    }, 1000);
+    
+    function getLocalStorage(key) {
+        let value = localStorage.getItem(key);
+        if (value) {
+            $(`#text${key}`).text(value);
+        }
+    }
+    
+    var saveBtn = $('.saveBtn');
+    saveBtn.on('click', function(){
+        let eventId = $(this).attr('id');
+        let eventText = $(this).parent().siblings().children('.appointment').val();
+        localStorage.setItem(eventId, eventText);
+    });
 });
