74b981f4ce0c7102434f5882ac2496fa00e0581a
NDPenaloza <ndpenaloza@gmail.com>
2020-10-31 19:54:57 -0700

    created row, columns, appended columns to row, and appended row to .container div

--- script.js
+++ script.js
@@ -1,47 +1,27 @@
 
-    console.log(moment());
-    // Display current day and time
-    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));
 
-        function updateColors(){
-            var currentTime = new Date().getHours();
-            for (var i = 9; i < 18; i++) { 
-            console.log(currentTime, $(`#${i}`).data("time"));
-            if ($(`#${i}`).data("time") == currentTime){
-                $(`#text${i}`).addClass( "present");
-            } else if (currentTime < $(`#${i}`).data("time")) {
-                $(`#text${i}`).addClass( "future");
-        }
+$(document).ready(function() {
+    // Current day and time in Jumbotron
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
+    // For loop iterating rows in .container div
+    for (let i = 9; i < 18; i++) {
+        // Create hourly-row
+        var hourlyRow = $('<div data-time=`${i}` id=`${i}` class="hourly-row">');
+        // Create first column for each hour
+        var hour = $('<div class="col-sm-1"> <p class="hour">' + formatAMPM(i) + '</p>');
+        // Create second column to record appointments
+        var appointment = $('<div class="col-sm-10 past"><textarea id=text${i} class="appointment"></textarea>');        
+        // Create third column for save button
+        var saveBtn = $('<div class="col-sm-1"><button class="saveBtn" id=`${i}`><i class="fas fa-save"></i></button>');
+        // append hour
+        hourlyRow.append(hour);
+        //append appointment
+        hourlyRow.append(appointment);
+        //append save button
+        hourlyRow.append(saveBtn);
+        // append hourlyRow to .container div
+        $(".container").append(hourlyRow);
     }
-}
-    
-    // Create row to display hour, info to save per hour, and a save button
 
 
-    //eventListener for save button
-    var saveBtn = $(".saveBtn");
-    saveBtn.on("click", function() {
-        var eventId = $(this).attr("id");
-        var eventText = $(this).find(".appointment").val();
-        localStorage.setItem(eventId, eventText);
-    })
-
-    // setAttribute to make every past hour gray, the current hour red, and future hour green
-
-    // Get Local Data
-    // var timeBlockArray = $(".hourly-row");
-    // timeBlockArray.each(function(index) {
-    //     var $timeBlock = $(timeBlockArray[index]).
-    //     // get the id
-    //     var id = $timeBlock.find("textarea").attr("id");
-    //     // get that data associated with the id
-    //     var data = localStorage.getItem(`hour-${id}`);
-    //     //update the textarea
-    //     if (data) {
-    //         timeBlockArray.find("textarea").val(data);
-    //     }   else {
-    //         timeBlockArray.find("textarea").val();
-    //     }
-    // });
-//$(document).ready(function() {
-//});
+});
